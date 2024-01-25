846973845fe4490b2100a75f75dfedec036c7a3e
yamazakiyoshiki <yoshiky1129@gmail.com>
2024-01-25 11:02:37 +0900

    ログイン画面完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 import Sidebar from './components/sidebar/Sidebar';
 import Chat from './components/chat/Chat';
+import Login from './components/login/Login';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { auth } from './firebase';
+import { login, logout } from './features/userSlice';
 
 function App() {
+
+  const user = useAppSelector((state) => state.user);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((loginUser) => {
+      if(loginUser) {
+        dispatch(
+          login({
+          uid: loginUser.uid,
+          photo: loginUser.photoURL,
+          email: loginUser.email,
+          displayName: loginUser.displayName,
+        }));
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch])
+
   return (
     <div className="App">
+      {user ?
+      (
+        <>
       {/* sidebar */}
         <Sidebar/>
       {/* chat */}
-      <Chat/>
+        <Chat/>
+        </>
+      ) :
+      (
+        <>
+        <Login/>
+        </>
+      )
+      }
     </div>
   );
 }
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -6,8 +6,11 @@ import SidebarChannel from './SidebarChannel'
 import MicIcon from "@mui/icons-material/Mic"
 import HeadphonesIcon from "@mui/icons-material/Headphones"
 import SettingIcon from "@mui/icons-material/Settings"
+import { auth } from '../../firebase'
+import { useAppSelector } from '../../app/hooks'
 
 const Sidebar = () => {
+  const user = useAppSelector((state) => state.user)
   return (
     <div className='sidebar'>
       {/* sidebarLeft */}
@@ -42,10 +45,10 @@ const Sidebar = () => {
           </div>
           <div className="sidebarFooter">
             <div className="sidebarAccount">
-              <img src="./icon.png" alt="" />
+              <img src={user?.photo} alt="" onClick={() => auth.signOut()}/>
               <div className="accountName">
-                <h4>YamaYoshi</h4>
-                <span>#8162</span>
+                <h4>{user?.displayName}</h4>
+                <span>#{user?.uid.substring(0, 4)}</span>
               </div>
             </div>
             <div className="sidebarVoice">

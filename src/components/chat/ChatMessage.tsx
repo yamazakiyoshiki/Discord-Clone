79c669f275d5c7458df87dda692987700e57c358
yamazakiyoshiki <yoshiky1129@gmail.com>
2024-01-25 23:37:37 +0900

    完成

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import "./ChatMessage.scss"
 import { Avatar } from '@mui/material'
+import { Timestamp } from 'firebase/firestore'
 
-const ChatMessage = () => {
+type Props = {
+  timestamp: Timestamp;
+  message: string;
+  user: {
+    uid: string;
+    photo: string;
+    email: string;
+    displayName: string;
+  }
+};
+
+const ChatMessage = ({message, user, timestamp}: Props) => {
   return (
     <div className='message'>
-      <Avatar/>
+      <Avatar src={user?.photo}/>
       <div className="messageInfo">
-        <h4>Yoshiki Yamazaki
+        <h4>{user?.displayName}
           <span className='messageTimestamp'>
-            2024/1/13
+            {new Date(timestamp?.toDate()).toLocaleString()}
           </span>
         </h4>
-        <p>メッセージ本文</p>
+        <p>{message}</p>
       </div>
     </div>
   )
